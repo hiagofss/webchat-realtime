ed5ea536ea136b9ba7636e342a35e199c5b6dd42
Hiago Souza <hiagofss98@gmail.com>
2019-09-11 23:05:53 -0300

    WebChat Realtime

--- index.js
+++ index.js
@@ -16,4 +16,16 @@ app.use('/', (req, res) => {
   res.render('index.html');
 });
 
+let messages = [];
+
+io.on('connection', socket => {
+  console.log(`Socket connect: ${socket.id}`);
+
+  socket.emit('previousMessages', messages);
+  socket.on('sendMessage', data => {
+    messages.push(data);
+    socket.broadcast.emit('receivedMessages', data);
+  });
+});
+
 server.listen(PORT);
